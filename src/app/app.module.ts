126276322bdde6e5b4029dcabc9ebf0ca36a4581
tomito26 <tommybwah@gmail.com>
2020-08-23 17:04:00 +0300

    added bootstrap and form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteMessageComponent } from './quote-message/quote-message.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteMessageComponent
+    QuoteMessageComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
